0868d6a699f83ae62c3d3ab9cc0692ed36ff1668
shreya-savliya <shreyasavliya71@gmail.com>
2024-09-27 16:17:20 -0400

    feat: set up the backend server structure

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import applyMiddlewares from "./middlewares/middlewares.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+connectDB();
+applyMiddlewares(app);
+
+app.listen(port, () => {
+    console.log("Server listening on port ", port)
+});
