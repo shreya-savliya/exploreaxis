353a12fb5faa3316dd3358fe0e73dbd1a308a06c
shreya-savliya <shreyasavliya71@gmail.com>
2024-09-30 18:21:24 -0400

    feat: :sparkles: add footer with responsive design, add transparent logo file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Box, ThemeProvider } from '@mui/material';
-import { lightTheme } from './styles/theme';
+import React from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import { lightTheme } from "./styles/theme";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
-      <Box>Hello</Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100%",
+          height: "100%",
+        }}
+      >
+        <Navbar />
+        <Box component={"main"} sx={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </Box>
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default App;
