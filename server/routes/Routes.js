c8231109cba3aa8df6e716a4af359b0ebb50b118
shreya-savliya <shreyasavliya71@gmail.com>
2024-10-02 12:51:07 -0400

    feat: :sparkles: implement search flight api

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get("/getflights", FlightController.GetAllFlights);
 router.get("/getflights/:id", FlightController.GetSingleFlight);
+router.post("/searchflights", FlightController.SearchFlights);
 
 export default router;
